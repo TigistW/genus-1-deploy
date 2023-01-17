cda0b8883ba2f0e83a5f33d575b71777ba0f27a5
Rediet-Ferew <redietasnakech@gmail.com>
2023-01-17 14:05:05 +0300

    intial project folder structure

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dru_iwg#6eas586r13a=p=e2t6h-szx(4y$i-y!vy5#@glwoa8'
+SECRET_KEY = 'django-insecure-6#t9dn*d#qo13dehnk!w(o&gsrgw54qd8rmbk$+(@)sli05jpi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ genus_frontend/android/app/src/main/kotlin/com/example/genus_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.genus_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
