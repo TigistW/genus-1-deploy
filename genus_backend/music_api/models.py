45439a0622ddf68b2045b142f0b86d794872b214
Rediet-Ferew <redietasnakech@gmail.com>
2023-01-18 10:27:16 +0300

    creating music model

--- genus_backend/music_api/models.py
+++ genus_backend/music_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length = 60)
+    artist = models.CharField(max_length = 60, default= None, blank=True, null=True)
+    length = models.DecimalField(decimal_places = 2, max_digits = 6)
+    genre = models.CharField(max_length = 20)
+    thumbnail = models.CharField(max_length = 200, default = None, blank=True, null=True)
+    
