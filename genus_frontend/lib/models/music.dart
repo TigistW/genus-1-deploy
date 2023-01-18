1db2c437886582c5fac9b9fcdf6c3d1877fdf0fd
TigistW <wondimnehtigist@gmail.com>
2023-01-18 10:56:39 +0300

    setup music model

--- genus_frontend/lib/models/music.dart
+++ genus_frontend/lib/models/music.dart
@@ -7,27 +7,30 @@ class Music {
   final String? genre;
   final String? image;
   final double? size;
+  final String? file;
 
   // the music instance constructor
-  Music({this.id, this.name, this.artist, this.size, this.genre, this.image});
+  Music({this.id, this.name, this.artist, this.size, this.genre, this.image, this.file});
 
 // a method that deserializes a json instance of the music model from the backend
   factory Music.fromJson(Map<String, dynamic> json) {
+
     return Music(
-      id: json['_id'],
-      name: json['name'] ?? "",
+      id: json['id'],
+      name: json['title'] ?? "",
       genre: json['genre'] ?? "",
       artist: json['artist'] ?? "",
-      size: json['size'] ?? "",
-      image: json['image'] ?? "",
+      size: json['length'] ?? "",
+      image: json['thumbnail'] ?? "",
+      file: json['song'] ?? "",
     );
   }
 
-   @override
-  String toString() =>
-      '{_id: $id, name:$name,artist:$artist, size: $size, genre:$genre, image: $image}';
-
+  // converting the instance of the class values into a string
   @override
-  List<Object?> get props =>
-      [id, name, artist, size, genre, image];
+  String toString() =>
+      '{id: $id, title:$name,artist:$artist, length: $size, genre:$genre, thumbnail: $image, song:$file}';
+
+//  the parameters as a list
+  List<Object?> get props => [id, name, artist, size, genre, image, file];
 }
