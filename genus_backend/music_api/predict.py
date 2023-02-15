02fff121308447ca82c550895d0979d6723d43b9
Rediet-Ferew <redietasnakech@gmail.com>
2023-02-15 12:35:31 +0300

    Predict error fix

--- genus_backend/music_api/predict.py
+++ genus_backend/music_api/predict.py
@@ -3,7 +3,7 @@ import os
 import pandas
 from django.conf import settings
 def predict_gen(meta_d):
-    path = os.path.join(settings.MODELS, 'models.p')
+    path = os.path.join(settings.MODELS, 'models.pkl')
     
     with open(path, 'rb') as pickled:
         data = pickle.load(pickled)
